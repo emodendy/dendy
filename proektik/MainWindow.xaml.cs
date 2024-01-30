00d0634877a2b767fda2fa168d5b8b903dc949bd
emodendy <mrdendy1337@mail.ru>
2024-01-30 10:20:28 +0300

    dfsfsdf

--- proektik/MainWindow.xaml.cs
+++ proektik/MainWindow.xaml.cs
@@ -20,9 +20,13 @@ namespace proektik
     /// </summary>
     public partial class MainWindow : Window
     {
-        float warbuffer = 0;
-        float rogbuffer = 0;
-        float wizbuffer = 0;
+        double hpwarbuffer = 0;
+        double hprogbuffer = 0;
+        double hpwizbuffer = 0;
+        double manawarbuffer = 0;
+        double manarogbuffer = 0;
+        double manawizbuffer = 0.5;
+
         Warrior warrior = new Warrior();
         Rogue rogue = new Rogue();
         Wizard wizard = new Wizard();
@@ -68,24 +72,93 @@ namespace proektik
 
         private void str_btn_Click(object sender, RoutedEventArgs e)
         {
-
             if (choice.SelectedItem.ToString() == "Warrior")
             {
-                warbuffer += 1;
-                warrior.strength = Convert.ToInt32(30 + warbuffer);
+                warrior.strength +=1;
                 lbStrength.Content = $"Strength: {warrior.strength}";
             }
             if (choice.SelectedItem.ToString() == "Rogue")
             {
-                rogbuffer += 1;
-                rogue.strength = Convert.ToInt32(20 + rogbuffer);
+                rogue.strength += 1;
                 lbStrength.Content = $"Strength: {rogue.strength}";
             }
             if (choice.SelectedItem.ToString() == "Wizard")
             {
-                wizbuffer += 1;
-                wizard.strength = Convert.ToInt32(15 + wizbuffer);
+                wizard.strength += 1;
                 lbStrength.Content = $"Strength: {wizard.strength}";
+            }
+        }
+
+        private void dex_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (choice.SelectedItem.ToString() == "Warrior")
+            {
+                warrior.dexterity += 1;
+                lbDexterity.Content = $"Dexterity: {warrior.dexterity}";
+            }
+            if (choice.SelectedItem.ToString() == "Rogue")
+            {
+                rogue.dexterity += 1;
+                lbDexterity.Content = $"Dexterity: {rogue.dexterity}";
+            }
+            if (choice.SelectedItem.ToString() == "Wizard")
+            {
+                rogue.dexterity += 1;
+                lbDexterity.Content = $"Dexterity: {rogue.dexterity}";
+            }
+        }
+
+        private void vit_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (choice.SelectedItem == "Warrior")
+            {
+                warrior.vitality += 1;
+                lbVitality.Content = $"Vitality: {warrior.vitality}";
+                warrior.health += 2;
+                HP.Content = $"HP: {warrior.health}";
+            }
+            if (choice.SelectedItem.ToString() == "Rogue")
+            {
+                rogue.vitality += 1;
+                lbVitality.Content = $"Vitality: {rogue.vitality}";
+                hprogbuffer += 1.5;
+                rogue.health = Convert.ToInt32(30 + hprogbuffer);
+                HP.Content = $"HP: {rogue.health}";
+            }
+            if (choice.SelectedItem.ToString() == "Wizard")
+            {
+                wizard.vitality += 1;
+                lbVitality.Content = $"Vitality: {wizard.vitality}";
+                hpwizbuffer += 1.4;
+                wizard.health = Convert.ToInt32(21 + hpwizbuffer);
+                HP.Content = $"HP: {wizard.health}";
+            }
+        }
+
+        private void int_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (choice.SelectedItem == "Warrior")
+            {
+                warrior.inteligence += 1;
+                lbInteligence.Content = $"Inteligence: {warrior.inteligence}";
+                warrior.mana += 1;
+                MANA.Content = $"MANA: {warrior.mana}";
+            }
+            if (choice.SelectedItem.ToString() == "Rogue")
+            {
+                rogue.inteligence += 1;
+                lbInteligence.Content = $"Inteligence: {rogue.inteligence}";
+                manarogbuffer += 1.2;
+                rogue.mana = Convert.ToInt32(18 + manarogbuffer);
+                MANA.Content = $"MANA: {rogue.mana}";
+            }
+            if (choice.SelectedItem.ToString() == "Wizard")
+            {
+                wizard.inteligence += 1;
+                lbInteligence.Content = $"Inteligence: {wizard.inteligence}";
+                manawizbuffer += 1.5;
+                wizard.mana = Convert.ToInt32(52 + manawizbuffer);
+                MANA.Content = $"MANA: {wizard.mana}";
             }
         }
     }
